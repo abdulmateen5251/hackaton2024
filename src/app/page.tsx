9ca1351a98904a0dc10375c8a8fe5b1f5bfc8400
abdulmateen5251 <abdulmateen5251@gmail.com>
2024-12-10 00:10:55 +0500

    Fix the UI and Add More Pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,9 @@ import Vita from './component/vita'
 import Part5 from './component/Part5'
 import Part6 from './component/Part6'
 import Header from './component/Header'
-import Navbar from './component/Navbar'
+
 import Work from './component/Work-with'
+import Navbar from './component/Navbar'
 
 
 
